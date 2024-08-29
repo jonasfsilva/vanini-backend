a365af070d02fdf25d68daa9314ae2eb2637d3cf
Jonas Ferreira <jonas.silva@grupomateus.com>
2024-08-29 09:40:12 -0300

    fix orders

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 
+from catalog.models import Category
+from catalog.models import Model
+from catalog.models import Product
+from catalog.models import ProductGalery
+from core.admin import BaseSuperAdmin
 
-@admin.register(Tag)
-class TagClass(BaseSuperAdmin):
+
+@admin.register(Category)
+class CategoryClass(BaseSuperAdmin):
+    ...
+    
+
+@admin.register(Model)
+class ModelClass(BaseSuperAdmin):
+    ...
+
+
+@admin.register(Product)
+class ProductClass(BaseSuperAdmin):
+    ...
+
+
+@admin.register(ProductGalery)
+class ProductGaleryClass(BaseSuperAdmin):
     ...
--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.admin import BaseSuperAdmin
+from orders.models import QuotationInfo
+
+
+@admin.register(QuotationInfo)
+class QuotationInfoClass(BaseSuperAdmin):
+    ...
